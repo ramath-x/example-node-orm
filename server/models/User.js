983593f0986f929d00d35b1aab2f9726617825bf
ramath kunasan <ramath.x36@gmail.com>
2024-07-02 12:07:01 +0700

    test model/index

--- server/models/User.js
+++ server/models/User.js
@@ -1,35 +1,44 @@
 const { Sequelize, DataTypes, cast } = require("sequelize");
 const { Address } = require("./Address");
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+require("dotenv").config();
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-});
+    dialect:
+      "mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  }
+);
 
+const db = {};
 
 module.exports = (sequelize) => {
-    const User = sequelize.define('users', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: true,
-            unique: true
+  const User = sequelize.define(
+    "users",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
-        }
-    }, {
-        timestamps: true
-    })
+  User.hasMany(Address);
 
-    User.hasMany(Address)
-
-    return User;
-}
-
-
+  return User;
+};
